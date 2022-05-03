23cfe66be06acdda642ed5237c6a709bb7c6c53f
AdamNB-sys <a1adnat@gmail.com>
2022-05-03 16:49:01 -0500

    task 1, took longer than i thought it would

---
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10000, 10000);
+        int lastNum = number % 10;
+        Console.Write("The last digit of " + number + " is ");
+        if (lastNum > 5)
+        {
+            Console.WriteLine(lastNum + " and is greater than 5");
+        }
+        else if (lastNum < 6 && lastNum != 0)
+        {
+            Console.WriteLine(lastNum + " and is less than 6 and not 0");
+        }
+        else if (lastNum == 0)
+        {
+            Console.WriteLine(lastNum + " and is 0");
+        }
+    }
+}
