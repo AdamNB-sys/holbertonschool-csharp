1681c5b74f4f97e7bd1f79e227c48da75b3816b4
AdamNB-sys <a1adnat@gmail.com>
2022-05-03 18:21:45 -0500

    task 5, 00 to 99

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i <= 99)
+            {
+                Console.Write($"{i:D2}");
+                if (i != 99)
+                {
+                    Console.Write(", ");
+                }
+                i++;
+            }
+        }
+    }
+}
