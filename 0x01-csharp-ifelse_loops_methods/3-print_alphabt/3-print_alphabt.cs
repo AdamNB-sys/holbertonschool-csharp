43df1e26dd283cfae29cfd206fc53ca20829062d
AdamNB-sys <a1adnat@gmail.com>
2022-05-03 17:54:48 -0500

    task 3, print alpha sans qe

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch;
+            for (ch = 'a'; ch <= 'z'; ch++)
+            {
+                if (ch == 'q' || ch == 'e')
+                {
+                    continue;
+                }
+                Console.Write(ch);
+            }
+        }
+    }
+}
