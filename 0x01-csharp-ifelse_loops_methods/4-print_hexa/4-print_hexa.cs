f5a03a6acc45ce34839ea5763d6df0c9a006765d
AdamNB-sys <a1adnat@gmail.com>
2022-05-03 18:11:12 -0500

    task 4, print hexadecimal

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i < 99)
+            {
+                Console.Write(i + " = 0x" + i.ToString("x") + "\n");
+                i++;
+            }
+        }
+    }
+}
