2e8cbbc94b2f3f07b3db98fd4c7ced4e6d8789a5
AdamNB-sys <a1adnat@gmail.com>
2022-05-03 20:48:47 -0500

    task 12, they still got us doin fizzbuzz

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _12_fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int x = 1; x < 101; x++)
+            {
+                if (x % 3 == 0)
+                    Console.Write("Fizz");
+                if (x % 5 == 0)
+                    Console.Write("Buzz");
+                if (x % 3 != 0 && x % 5 != 0)
+                    Console.Write(x);
+                Console.Write(" ");
+            }
+        }
+    }
+}
