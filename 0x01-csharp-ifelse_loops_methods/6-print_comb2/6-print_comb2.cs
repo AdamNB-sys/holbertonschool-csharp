8502f9c167f1454473296654c1bc2c09a11658d6
AdamNB-sys <a1adnat@gmail.com>
2022-05-03 19:00:56 -0500

    task 6, print other combination

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            for (x = 0; x < 8; x++)
+            {
+                for (y = 0; y <= 9; y++)
+                {
+                    if (x < y)
+                    {
+                        Console.Write("{0}{1}, ", x, y);
+                    }
+                }
+            }
+            Console.WriteLine("89");
+        }
+    }
+}
