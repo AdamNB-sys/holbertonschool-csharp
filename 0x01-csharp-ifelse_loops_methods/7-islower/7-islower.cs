0bc8fecfdccc5abfbdc99af00d5fdeb18c407e98
AdamNB-sys <a1adnat@gmail.com>
2022-05-03 19:27:26 -0500

    task 7, uppercase lowercase

---
+++ 0x01-csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Character
+{
+    public static bool IsLower(char c)
+    {
+        if (c < 122 && c > 96)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
