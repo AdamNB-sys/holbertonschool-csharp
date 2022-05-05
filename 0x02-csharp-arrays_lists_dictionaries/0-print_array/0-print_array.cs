cb41d59b58ad0dfc958eaf26065c79dad95cf83f
AdamNB-sys <a1adnat@gmail.com>
2022-05-04 21:35:13 -0500

    task 0, print array of ints

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -9,14 +9,21 @@ class Array
         // integers start with 0 and inc by 1 until size is met
         // if size == 0 -> blank line
         // if size < 0 -> "Size cannot be negative" return null
-        if (size > 0)
+        if (size >= 0)
         {
             int[] ArrCopy = new int[size];
-            for (int i = 0; i < ArrCopy; i++)
+            for (int i = 0; i < ArrCopy.Length; i++)
             {
-                
+                ArrCopy[i] = i;
+                Console.Write("{0} ", ArrCopy[i]);
             }
+            Console.Write("\n");
+            return ArrCopy;
         }
-        
+        else
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
     }
 }
