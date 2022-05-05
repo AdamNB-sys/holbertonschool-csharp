45a13d3df9a5611fbe33594319fb0835e47ef3f4
AdamNB-sys <a1adnat@gmail.com>
2022-05-05 10:15:27 -0500

    task 3, reverse array

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        if ( array != null && array.Length > 0)
+        {
+            int i = array.Length - 1;
+            while (i > 0)
+            {
+                Console.Write($"{array[i]} ");
+                i--;
+            }
+            Console.Write(array[0]);
+        }
+        Console.Write("\n");
+    }
+}
