ba6c59c942a0a52e3f9268dc5df8181a8966016c
AdamNB-sys <a1adnat@gmail.com>
2022-05-04 20:05:05 -0500

    task 0 progress

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        // returns new array
+        // integers start with 0 and inc by 1 until size is met
+        // if size == 0 -> blank line
+        // if size < 0 -> "Size cannot be negative" return null
+        if (size > 0)
+        {
+            int[] ArrCopy = new int[size];
+            for (int i = 0; i < ArrCopy; i++)
+            {
+                
+            }
+        }
+        
+    }
+}
