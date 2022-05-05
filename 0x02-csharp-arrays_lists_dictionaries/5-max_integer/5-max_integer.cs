626bf793c2709e9e4da763d386dc5cbe5c30f7fd
AdamNB-sys <a1adnat@gmail.com>
2022-05-05 15:05:34 -0500

    task 5, find biggest number

---
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class List
+{
+    public static int MaxInteger(List<int> myList)
+    {
+        if (myList.Count > 0)
+        {
+            int max = myList[0];
+            foreach (int i in myList)
+            {
+                if (i > max)
+                    max = i;
+            }
+            return max;
+        }
+        Console.WriteLine("List is empty");
+        return -1;
+    }
+}
+
