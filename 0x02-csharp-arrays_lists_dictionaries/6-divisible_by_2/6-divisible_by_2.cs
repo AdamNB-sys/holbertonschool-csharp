a923ef0a35e2683839f2ffe6b41de768cd85bcb5
AdamNB-sys <a1adnat@gmail.com>
2022-05-05 15:30:51 -0500

    task 6, find multiples of 2

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> evens = new List<bool>();
+        foreach (int i in myList)
+        {
+            if (i % 2 == 0)
+                evens.Add(true);
+            else
+                evens.Add(false);
+        }
+        return evens;
+    }
+}
