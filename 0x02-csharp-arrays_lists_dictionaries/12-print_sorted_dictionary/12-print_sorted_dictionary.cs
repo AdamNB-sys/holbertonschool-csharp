afd6e7ea41de29ccdd5bf90d790c81d8b2a246ba
AdamNB-sys <a1adnat@gmail.com>
2022-05-05 22:22:46 -0500

    task 12, sort dict in ascending order

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        SortedList<string, string>  ascDict = new SortedList<string, string>();
+        foreach (KeyValuePair<string, string> pair in myDict)
+        {
+            // multiDict[pair.Key] = pair.Value * 2;
+            ascDict[pair.Key] = pair.Value;
+        }
+        foreach (KeyValuePair<string, string> pair in ascDict)
+        {
+            Console.WriteLine($"{pair.Key}: {pair.Value}");
+        }
+    }
+}
