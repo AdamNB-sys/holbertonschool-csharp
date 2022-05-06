f392406aa4b81c5016c265124465e447ad8aee12
AdamNB-sys <a1adnat@gmail.com>
2022-05-05 19:11:44 -0500

    task 7, delete at... i'm so behind

---
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class List
+{
+    public static List<int> DeleteAt(List<int> myList, int index)
+    {
+        // (index < array.Length && index >= 0)
+        if (index < myList.Count && index >= 0)
+        {
+            myList.Remove(myList[index]);
+        }
+        else
+        {
+            Console.WriteLine("Index is out of range");
+        }
+        return myList;
+    }
+}
