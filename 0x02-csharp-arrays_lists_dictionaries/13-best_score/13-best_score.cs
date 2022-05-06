173e75677da4bd72e16011ce999f3a638894d3ed
AdamNB-sys <a1adnat@gmail.com>
2022-05-05 22:53:53 -0500

    task 13, best score

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        int winner = 0;
+        string keyName = "";
+        if (myList != null && myList.Count > 0)
+        {
+            foreach (KeyValuePair<string, int> pair in myList)
+            {
+                if (winner <= pair.Value)
+                {
+                    winner = pair.Value;
+                    keyName = pair.Key;
+                }
+            }
+            return keyName;
+        }
+        else
+        {
+            return "None";
+        }
+    }
+}
