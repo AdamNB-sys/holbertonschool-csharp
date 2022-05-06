e3112d9c2853ff981463ccff5ab4664b45d3f5f6
AdamNB-sys <a1adnat@gmail.com>
2022-05-05 20:02:46 -0500

    task 8, return number of keys

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        List<string> keyList = new List<string>();
+        foreach (var key in myDict.Keys)
+        {
+            keyList.Add(key);
+        }
+        return keyList.Count;
+    }
+}
+
