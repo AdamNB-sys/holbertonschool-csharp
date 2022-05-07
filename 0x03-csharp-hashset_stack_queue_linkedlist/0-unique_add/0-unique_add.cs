dce1f19f7d0953568018beb2266b75e5beb64a56
AdamNB-sys <a1adnat@gmail.com>
2022-05-06 21:02:17 -0500

    task 0, add unique numbers

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        HashSet<int> uniNumbers = new HashSet<int>();
+        foreach (int item in myList)
+        {
+            uniNumbers.Add(item);
+        }
+        int total = 0;
+        foreach (int item in uniNumbers)
+        {
+            total += item;
+        }
+        return total;
+    }
+}
