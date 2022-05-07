654a5f35652882acf5783548a31b80cde2081d90
AdamNB-sys <a1adnat@gmail.com>
2022-05-06 23:56:48 -0500

    task 4, queue, it's basically a stack

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: {0}", aQueue.Count);
+        if (aQueue.Count > 0)
+        {
+            Console.WriteLine("First item: {0}", aQueue.Peek());
+        }
+        else
+        {
+            Console.WriteLine("Stack is empty");
+        }
+        Console.WriteLine("Stack contains \"{0}\": {1}", search, aQueue.Contains(search));
+        while (aQueue.Contains(search))
+        {
+            aQueue.Dequeue();
+        }
+        aQueue.Enqueue(newItem);
+        return aQueue;
+    }
+}
