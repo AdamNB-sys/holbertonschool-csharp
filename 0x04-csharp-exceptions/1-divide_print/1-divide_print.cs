3324a48efe4b5ab9da72771142be9e85991ca60b
AdamNB-sys <a1adnat@gmail.com>
2022-05-09 17:15:33 -0500

    task 1, safe integer division

---
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Int
+{
+    public static void divide(int a, int b)
+    {
+        int sum = 0;
+        try
+        {
+            sum = a / b;
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Cannot divide by zero");
+        }
+        finally
+        {
+            Console.WriteLine($"{a} / {b} = {sum}");
+        }
+    }
+}
+
