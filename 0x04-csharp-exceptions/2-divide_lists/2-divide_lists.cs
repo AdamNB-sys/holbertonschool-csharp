775a9b0bc15ba5a5a822390d34df806251eb893c
AdamNB-sys <a1adnat@gmail.com>
2022-05-09 18:02:47 -0500

    task 3, safe list division

---
+++ 0x04-csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+class List
+{
+    public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
+    {
+        List<int> sumList = new List<int>();
+        for (int i = 0; i < listLength; i++)
+        {
+            try
+            {
+                int sum = list1[i] / list2[i];
+                sumList.Add(sum);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cannot divide by zero");
+                sumList.Add(0);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Out of range");
+            }
+        }
+        return sumList;
+    }
+}
