a145dcda81be1cf9b9dfe9fbfc3bb2ce6e0ecd9c
AdamNB-sys <a1adnat@gmail.com>
2022-05-11 20:53:58 -0500

    task 3, privatized health added public getter method

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary> Empty public enemy class for a zombie </summary>
+    public class Zombie
+    {
+        /// private health field for class zombie
+        private int _health;
+
+        ///<summary> public class constructor zombie that sets health to 0 </summary>
+        public Zombie()
+        {
+            _health = 0;
+        }
+
+        ///<summary> new public class constructor that ensures health is at least 0</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+                throw new ArgumentException(String.Format("Health must be greater than or equal to 0"));
+            else
+                _health = value;
+        }
+
+        ///<summary> Public getter for health </summary>
+        public int GetHealth()
+        {
+            return _health;
+        }
+    }
+}
