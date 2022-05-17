41efc9589b7b6bc0e1abbe055763c5cfeef62496
AdamNB-sys <a1adnat@gmail.com>
2022-05-16 22:39:01 -0500

    task 5, throw exception

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+/// <summary> Class shape </summary>
+class Shape
+{
+    /// <summary> Public method to set area </summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
