5b5349fb3335e473abce54e48347eb4cb943851e
AdamNB-sys <a1adnat@gmail.com>
2022-05-16 22:05:25 -0500

    task 3, print methods and properties

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+
+/// <summary> Class for testing objects </summary>
+class Obj
+{
+    /// <summary> Method prints the names of properties, and object methods </summary>
+    public static void Print(object myObj)
+    {
+        string name = myObj.GetType().Name;
+
+        Console.WriteLine($"{name} Properties:");
+        foreach (var item in myObj.GetType().GetProperties())
+        {
+            Console.WriteLine(item.Name);
+        }
+        Console.WriteLine($"{name} Methods");
+        foreach (var item in myObj.GetType().GetMethods())
+        {
+            Console.WriteLine(item.Name);
+        }
+    }
+}
+
