8dde64cbeef407171d2e8a0fbe6afc6b6882df14
AdamNB-sys <a1adnat@gmail.com>
2022-05-16 20:21:38 -0500

    adding xml documentation

--- 0x08-csharp-inheritance/0-is/0-is.cs
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 
-
+/// <summary> Type checking class for integers. </summary>
 class Obj
 {
+    /// <summary> Tests if the incoming object is an integer </summary>
     public static bool IsOfTypeInt(object obj)
     {
         if (obj is int)
