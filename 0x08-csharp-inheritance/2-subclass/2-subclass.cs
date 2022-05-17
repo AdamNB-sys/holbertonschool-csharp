5f514151c81c74d93630e8cbb0dc239aeefd2fa7
AdamNB-sys <a1adnat@gmail.com>
2022-05-16 20:54:38 -0500

    task 2, is subclass of

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+/// <summary> Class for checking if an object is an instance of a class that inherits from another class </summary>
+class Obj
+{
+    /// <summary> Method returns true if object is instance of inherited class, otherwise returns false </summary>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        if (derivedType.IsSubclassOf(baseType))
+            return true;
+        else
+            return false;
+    }
+}
