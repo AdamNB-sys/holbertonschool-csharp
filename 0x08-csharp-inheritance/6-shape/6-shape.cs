3b8d44dcb39b3522f2a253e5737bb0a9ea808eda
AdamNB-sys <a1adnat@gmail.com>
2022-05-16 23:05:05 -0500

    task 6, rectangle inherits from shape

---
+++ 0x08-csharp-inheritance/6-shape/6-shape.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+/// <summary> Base class Shape </summary>
+class Shape
+{
+    /// <summary> Public method to set area </summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+/// <summary> Subclass Rectangle with inheritance from Shape </summary>
+class Rectangle : Shape
+{
+    /// Private field width
+    private int _width;
+    /// Private field height
+    private int _height;
+
+    /// <summary> Public getter and setter for width </summary>
+    public int Width
+    {
+        get { return _width; }
+        set
+        {
+            if (value < 0) { throw new ArgumentException("Width must be greater than or equal to 0"); }
+            else _width = value;
+        }
+    }
+
+    /// <summary> Public getter and setter for height </summary>
+    public int Height
+    {
+        get { return _height; }
+        set
+        {
+            if (value < 0) { throw new ArgumentException("Height must be greater than or equal to 0"); }
+            else _height = value;
+        }
+    }
+}
