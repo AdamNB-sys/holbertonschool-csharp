0b07ab2a8d4bc08c17c8aca90d5bae777675e66b
AdamNB-sys <a1adnat@gmail.com>
2022-05-23 14:45:45 -0500

    task 3, calculate vector magnitude

--- 0x00-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x00-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
 
 class VectorMath
 {
     public static double Magnitude(double[] vector)
     {
-        for (int i = 0; i < vector.Length; i++)
+        if (vector.Length == 2)
         {
-            Console.WriteLine(i);
+            double result = (Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2)));
+            return (Math.Round(result, 2));
+        }
+        else if (vector.Length == 3)
+        {
+            double result = (Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2)));
+            return (Math.Round(result, 2));
+        }
+        else
+        {
+            return -1;
         }
     }
 }
--- 0x00-csharp-linear_algebra/3-magnitude/main.cs
+++ 0x00-csharp-linear_algebra/3-magnitude/main.cs
@@ -5,5 +5,6 @@ class Program
     static void Main()
     {
         double[] vector = {9, -7, 4};
+        Console.WriteLine(VectorMath.Magnitude(vector));
     }
 }
