78b585febba83c551825fb245241fc31a5c35285
AdamNB-sys <a1adnat@gmail.com>
2022-05-23 15:19:27 -0500

    it compiles and runs!

--- 0x00-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ 0x00-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -11,11 +11,11 @@ class VectorMath
         double[] result = new double[1] {-1};
         if (vector1.Length == 2 && vector2.Length == 2)
         {
-            result = new double[2] {((vector1[0] + vector2[0]), (vector1[1] + vector2[1]))};
+            result = new double[2] {(vector1[0] + vector2[0]), (vector1[1] + vector2[1])};
         }
         else if (vector1.Length == 3 && vector2.Length == 3)
         {
-            result = new double[3] {((vector1[0] + vector2[0]), (vector1[1] + vector2[1]), (vector1[2] + vector2[2]))};
+            result = new double[3] {(vector1[0] + vector2[0]), (vector1[1] + vector2[1]), (vector1[2] + vector2[2])};
         }
         return result;
     }
