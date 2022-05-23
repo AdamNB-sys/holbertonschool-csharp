ea11716107cdcc31d48b19c76dd23716b720b349
AdamNB-sys <a1adnat@gmail.com>
2022-05-23 16:25:05 -0500

    reassigning product to 0 inside if statement

--- 0x00-csharp-linear_algebra/12-dot_product/12-dot_product.cs
+++ 0x00-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -10,6 +10,7 @@ class VectorMath
         double product = -1;
         if ((vector1.Length == 2 || vector1.Length == 3) && (vector1.Length == vector2.Length))
         {
+            product = 0;
             for (int i = 0; i < vector1.Length; i++)
             {
                 product += vector1[i] * vector2[i];
