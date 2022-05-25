b9eea71f98392b1f2886ffc780a456b673d30d99
AdamNB-sys <a1adnat@gmail.com>
2022-05-24 21:53:41 -0500

    commit

---
+++ 0x01-csharp-generics/1-enqueue/queue.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+/// <summary> Generic class Queue </summary>
+class Queue<T>
+{
+    /// <summary> Gets type of object </summary>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+}
