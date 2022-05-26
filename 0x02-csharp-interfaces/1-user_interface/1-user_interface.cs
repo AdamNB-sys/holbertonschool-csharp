40b30ae9bf6ea44f89c380808a3b77164b80aef0
AdamNB-sys <a1adnat@gmail.com>
2022-05-25 21:17:43 -0500

    task 1, create user interface

---
+++ 0x02-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+
+/// <summary> Abstract class Base </summary>
+abstract class Base
+{
+    public string name { get; set; }
+
+    public override string ToString()
+    {
+        return $"{name} is a {base.GetType()}";
+    }
+}
+
+interface IInteractive
+{
+    void Interact();
+}
+
+interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+interface ICollectable
+{
+    bool IsCollected { get; set; }
+    void Collect();
+}
+
+class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+    public int durability { get; set; }
+    public bool IsCollected { get; set; }
+    public void Interact()
+    {
+        return;
+    }
+    public void Break()
+    {
+        return;
+    }
+    public void Collect()
+    {
+        return;
+    }
+}
