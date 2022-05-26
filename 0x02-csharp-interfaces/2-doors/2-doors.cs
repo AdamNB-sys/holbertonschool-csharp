6c09a17ca8f3b4b4c3723ac84f1e15230164ef1e
AdamNB-sys <a1adnat@gmail.com>
2022-05-25 22:34:09 -0500

    task 2, the door is locked

---
+++ 0x02-csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+/// <summary> Abstract class Base </summary>
+abstract class Base
+{
+    public string name { get; set; }
+
+    public override string ToString()
+    {
+        return $"{name} is a {base.GetType()}";
+    }
+}
+
+interface IInteractive
+{
+    void Interact();
+}
+
+interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+
+/// <summary> Class Door with inheritance form base and interactive </summary>
+class Door : Base, IInteractive
+{
+    public Door(string itemName = "Door")
+    {
+        name = itemName;
+    }
+
+    public void Interact()
+    {
+        Console.WriteLine($"You try to open the {name}. It's locked.");
+    }
+}
