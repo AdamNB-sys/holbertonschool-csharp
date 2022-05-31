b7db5c63d3f144f633c370739c4fbc8ef99f3d9b
AdamNB-sys <a1adnat@gmail.com>
2022-05-31 16:17:49 -0500

    sure

--- 0x01-csharp-generics/1-enqueue/queue.cs
+++ 0x01-csharp-generics/1-enqueue/queue.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 
 
 /// <summary> Generic class Queue </summary>
 class Queue<T>
 {
+    public Node head = null;
+    public Node Tail = null;
+    public int count = 0;
+
     /// <summary> Gets type of object </summary>
     public Type CheckType()
     {
@@ -13,12 +18,25 @@ class Queue<T>
     /// <summary> Creates nodes for queue </summary>
     public class Node
     {
-        public var value = null;
+        public T value = default(T);
         public Node next = null;
 
-        public Node(var value)
+        public Node(T val)
         {
-            this.value = value;
+            value = val;
         }
+    }
+
+    /// <summary> creates a new node and adds it to the end of the queue </summary>
+    public void Enqueue(T val)
+    {
+        public Node newNode = new Node(T val);
+        count += 1;
+    }
+
+    /// <summary> Count returns the number of nodes in the queue </summary>
+    public void Count()
+    {
+        return count;
     }
 }
