1cc1a90633e2577b5cb5c3e2d7588835bb4a43b0
AdamNB-sys <a1adnat@gmail.com>
2022-09-17 19:01:07 -0500

    vector scalar multiplication

---
+++ 0x00-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+/// <summary>
+/// Class Vector Math does scalar multiplication on 2D and 3D vectors
+/// </summary>
+class VectorMath
+{
+    /// <summary> Takes in 2D or 3D vector and multiplies it by the passed scalar </summary>
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        if (vector.Length == 2 || vector.Length == 3)
+        {
+            for (int i = 0; i < vector.Length; i++)
+            {
+                vector[i] *= scalar;
+            }
+            return vector;
+        }
+        else
+        {
+            return new double[1] { -1 };
+        }
+    }
+}
