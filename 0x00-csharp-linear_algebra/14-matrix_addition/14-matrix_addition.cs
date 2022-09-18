4a7af6db15906fcea1c9f6b6d10872b83c10c385
AdamNB-sys <a1adnat@gmail.com>
2022-09-17 22:34:36 -0500

    some of task 14, stopping for the night

---
+++ 0x00-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/// <summary> Class MatrixMath performs computations with matrices </summary>
+class MatrixMath
+{
+    public static double[,] Add(double[,] matrix1, double[,] matrix2)
+    {
+        // set up new matrix
+        // compare lengths of matrices
+        // ensure 2D or 3D matrices only
+        // iterate the list of lists
+        // add each corresponding element and put the sum
+        // in the correct matrix coordinate
+    }
+}
+
