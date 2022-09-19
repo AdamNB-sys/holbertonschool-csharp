8f198286267a1a190fd3caacc83b7ed2a32f7f15
AdamNB-sys <a1adnat@gmail.com>
2022-09-19 18:32:52 -0500

    still need if check

--- 0x00-csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
+++ 0x00-csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -6,8 +6,8 @@ class MatrixMath
 {
     public static double[,] Shear2D(double[,] matrix, char direction, double factor)
     {
-        //if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
-        //    return new double[,] {{-1}};
+        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+            return new double[,] {{-1}};
 
         switch (direction)
         {
