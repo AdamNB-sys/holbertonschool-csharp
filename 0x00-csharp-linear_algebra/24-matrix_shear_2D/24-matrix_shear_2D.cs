45a2e6b7a4e9fbebbbe71b8e5c575ac98a47a11e
AdamNB-sys <a1adnat@gmail.com>
2022-09-19 18:28:51 -0500

    works for x and bad data but not y

---
+++ 0x00-csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+/// <summary> Class MatrixMath performs mathematical functions on matrices </summary>
+class MatrixMath
+{
+    public static double[,] Shear2D(double[,] matrix, char direction, double factor)
+    {
+        //if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+        //    return new double[,] {{-1}};
+
+        switch (direction)
+        {
+            case 'x':
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    matrix[i, 0] += matrix[i, 1] * factor;
+                }
+                return matrix;
+            case 'y':
+                for (int i = 0; i < matrix.GetLength(1); i++)
+                {
+                    matrix[i, 1] += matrix[i, 0] * factor;
+                }
+                return matrix;
+            default:
+                return new double[,] {{-1}};
+        }
+    }
+}
+
