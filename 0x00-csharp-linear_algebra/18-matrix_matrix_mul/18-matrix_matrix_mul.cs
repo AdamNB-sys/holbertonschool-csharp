50992930df31daf8f9255d104e2c43f42bbb51c7
AdamNB-sys <a1adnat@gmail.com>
2022-09-19 15:16:41 -0500

    i don't know how to do math

--- 0x00-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
+++ 0x00-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -7,7 +7,7 @@ class MatrixMath
     public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
     {
         double[,] matrixResult;
-        if (matrix1.GetLength(0) != matrix2.GetLength(1))
+        if (matrix1.GetLength(1) != matrix2.GetLength(0))
         {
             matrixResult = new double[1, 1]{{-1}};
             return matrixResult;
@@ -15,14 +15,14 @@ class MatrixMath
         else
         {
             matrixResult = new double[matrix1.GetLength(0), matrix2.GetLength(1)];
-            for (int i = 0; i < matrixResult.GetLength(0); i++)
+            for (int i = 0; i < matrixResult.GetLength(0); i++) // rows of matrixResult
             {
-                for (int j = 0; j < matrixResult.GetLength(1); j++)
+                for (int j = 0; j < matrixResult.GetLength(1); j++) // columns of matrixResult
                 {
                     matrixResult[i, j] = 0;
                     for (int k = 0; k < matrix1.GetLength(1); k++)
                     {
-                        matrixResult[i, j] = matrix1[i, k] * matrix2[k, j];
+                        matrixResult[i, j] += matrix1[i, k] * matrix2[k, j];
                     }
                 }
             }
