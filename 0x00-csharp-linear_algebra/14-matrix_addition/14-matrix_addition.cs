812c3904e21159b0b928b5945114f0062429e871
AdamNB-sys <a1adnat@gmail.com>
2022-09-19 11:01:55 -0500

    task 14, matrix addition

--- 0x00-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x00-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -3,14 +3,33 @@
 /// <summary> Class MatrixMath performs computations with matrices </summary>
 class MatrixMath
 {
+    /// <summary>
+    /// This method adds two matrices and returns the result as a matrix
+    /// </summary>
     public static double[,] Add(double[,] matrix1, double[,] matrix2)
     {
-        // set up new matrix
-        // compare lengths of matrices
-        // ensure 2D or 3D matrices only
-        // iterate the list of lists
-        // add each corresponding element and put the sum
-        // in the correct matrix coordinate
+        double[,] matrixResult;
+        if ((matrix1.GetLength(0) == 2 && matrix1.GetLength(1) == 2) && matrix1.GetLength(0) == matrix2.GetLength(0))
+        {
+            matrixResult = new double[2,2];
+        }
+        else if ((matrix1.GetLength(0) == 3 && matrix1.GetLength(1) == 3) && matrix1.GetLength(0) == matrix2.GetLength(0))
+        {
+            matrixResult = new double[3,3];
+        }
+        else
+        {
+            matrixResult = new double[,]{{-1}};
+        }
+
+        for (int i = 0; i < matrix1.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix1.GetLength(1); j++)
+            {
+                matrixResult[i, j] = matrix1[i, j] + matrix2[i, j];
+            }
+        }
+        return matrixResult;
     }
 }
 
