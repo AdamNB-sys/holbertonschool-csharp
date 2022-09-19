4c314ef41f90823c1b3ec3bc6713f87a431c36f3
AdamNB-sys <a1adnat@gmail.com>
2022-09-19 11:49:40 -0500

    messy but i think it works

---
+++ 0x00-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+/// <summary> Class MatrixMath performs mathematical functions on matrices </summary>
+class MatrixMath
+{
+    public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+    {
+        double[,] matrixScaled;
+        if ((matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2))
+        {
+            matrixScaled = new double[2,2];
+        }
+        else if ((matrix.GetLength(0) == 3 && matrix.GetLength(1) == 3))
+        {
+            matrixScaled = new double[3,3];
+        }
+        else
+        {
+            matrixScaled = new double[1, 1]{{-1}};
+            return matrixScaled;
+        }
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                matrix[i, j] *= scalar;
+            }
+        }
+        return matrix;
+    }
+}
+
