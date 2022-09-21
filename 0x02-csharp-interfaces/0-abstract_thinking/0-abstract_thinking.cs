e2173261410c731f53c4808cfede98b6775874e5
AdamNB-sys <a1adnat@gmail.com>
2022-09-21 17:11:16 -0500

    even more xml

--- 0x02-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
+++ 0x02-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -4,10 +4,10 @@
 /// <summary> Abstract Base class </summary>
 public abstract class Base
 {
-    /// <summary> string name </summary>
+    /// <summary> string name is name of base </summary>
     public string name = "";
 
-    /// <summary> ToString method </summary>
+    /// <summary> ToString method: name + is a + basedotGetType </summary>
     public override string ToString()
     {
         return name + " is a " + base.GetType();
