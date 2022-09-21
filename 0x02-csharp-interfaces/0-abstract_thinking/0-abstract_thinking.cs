fcd1e082a3a617f665a6e87540bfc46a14d5e689
AdamNB-sys <a1adnat@gmail.com>
2022-09-21 17:09:46 -0500

    XML everywhere to appease the compiler gods

---
+++ 0x02-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+/// <summary> Abstract Base class </summary>
+public abstract class Base
+{
+    /// <summary> string name </summary>
+    public string name = "";
+
+    /// <summary> ToString method </summary>
+    public override string ToString()
+    {
+        return name + " is a " + base.GetType();
+    }
+}
