a90e7c57dce1279a2aa05847785538a446f1b4ec
AdamNB-sys <a1adnat@gmail.com>
2022-09-21 16:57:34 -0500

    task 0, abstract base class

---
+++ 0x02-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+/// <summary> Abstract Base class </summary>
+public abstract class Base
+{
+    public string name = "";
+
+    public override string ToString()
+    {
+        return name + " is a " + base.GetType();
+    }
+}
