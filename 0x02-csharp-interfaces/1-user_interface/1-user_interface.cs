16c319a69f58fd7fd6f0dba4172228f00d58fe08
AdamNB-sys <a1adnat@gmail.com>
2022-09-21 17:36:00 -0500

    task 1 strange warning but it gives correct output

---
+++ 0x02-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+
+/// <summary> Abstract Base class </summary>
+public abstract class Base
+{
+    /// <summary> string name is name of base </summary>
+    public string name = "";
+
+    /// <summary> ToString method: name + is a + basedotGetType </summary>
+    public override string ToString()
+    {
+        return name + " is a " + base.GetType();
+    }
+}
+
+/// <summary> Interface IInteractive to call interact method </summary>
+public interface IInteractive
+{
+    void Interact();
+}
+
+/// <summary> Interface IBreakable to set durability and breakability </summary>
+public interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+/// <summary> Interface ICollectable determines collectability of object </summary>
+public interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+/// <summary> Class TestObject inherits from Base and all 3 interfaces </summary>
+public class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+    public int durability { get; set; }
+    public bool isCollected { get; set; }
+    public string name { get; set; }
+
+    public void Interact()
+    {
+        return;
+    }
+
+    public void Break()
+    {
+        return;
+    }
+
+    public void Collect()
+    {
+        return;
+    }
+}
